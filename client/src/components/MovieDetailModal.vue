38bd6e465e2a5119e320edae253a629912ad3e5e
배중권 <zsecq2@naver.com>
2022-11-21 00:45:36 +0900

    디테일 마무리 - 배중권

--- client/src/components/MovieDetailModal.vue
+++ client/src/components/MovieDetailModal.vue
@@ -2,7 +2,7 @@
 
   <v-card class="movie-detail-modal" tile>
     <v-card-title>
-      <span class="headline">{{ movie.title }}</span><br>
+      <span class="headline">{{ movie.title }}</span>
       <span v-if="movie.title_en">({{ movie.title_en }})</span>
     </v-card-title>
 
@@ -11,22 +11,65 @@
 
       <v-divider></v-divider>
       <v-row justify="space-around">
-        <v-col cols="3">
+        <v-col v-if="!video" cols="4">
           <v-img
             :src="movie.img_url"
             gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
           >
           </v-img>
         </v-col>
+        <v-col v-if="video" cols="4">
+          <iframe id="player" type="text/html" width="640" height="450"
+          :src="videoSrc" frameborder="0"></iframe>
+        </v-col>
+        <v-col cols="4">
+          <VideoSearch
+            :movie="movie"
+            @goApp="goApp"
+          />
+        </v-col>
       </v-row>
 
       <v-divider></v-divider>
+      <v-row justify="space-around">
+        <v-col cols="4">
+          <v-card-text>
+            <div><b>감독</b> | <span v-for="name in directors" :key=name>{{ name }}, </span></div>
+            <div><b>배우</b> | <span v-for="name in actors" :key=name>{{ name }}, </span></div>
+            <div><b>평점</b> | <span>{{ movie.rate }} / 10</span></div>
+          </v-card-text>
+        </v-col>
+        <v-col cols="4">
+          <v-card-text>
+            <div><b>평가</b></div>
+            <ul style="list-style: none;">
+              <li v-for="review in reviews" :key=review.id>
+                <v-row>
+                  <v-rating
+                    :value="review.score"
+                    background-color="white"
+                    color="yellow accent-4"
+                    dense
+                    size="8"
+                    readonly
+                  ></v-rating>
+                  <div>
+                    | {{ review.content }}
+                    <i style="font-size:12px;"> by {{ review.username }}</i>
+                  </div>
+                  <div v-if="user_pk==review.user" >
+                    <v-btn small v-on:click="reviewDelete($event, review)">
+                      삭제
+                    </v-btn>
+                  </div>
 
-      <v-card-text>
-        <div><b>감독</b> | <span v-for="name in directors" :key=name>{{ name }}, </span></div>
-        <div><b>배우</b> | <span v-for="name in actors" :key=name>{{ name }}, </span></div>
-        <div><b>평점</b> | <span>{{ movie.rate }}</span></div>
-      </v-card-text>
+                </v-row>
+              </li>
+            </ul>
+
+          </v-card-text>
+        </v-col>
+      </v-row>
 
       <v-divider></v-divider>
 
@@ -39,7 +82,7 @@
         </v-expansion-panel>
       </v-expansion-panels>
       
-      <v-card-text>
+      <!-- <v-card-text>
         <div><b>평가</b></div>
         <ul style="list-style: none;">
           <li v-for="review in reviews" :key=review.id>
@@ -66,7 +109,7 @@
           </li>
         </ul>
 
-      </v-card-text>
+      </v-card-text> -->
 
     </v-container>
 
@@ -80,6 +123,7 @@
 <script>
 import axios from 'axios'
 import jwtDecode from 'jwt-decode'
+import VideoSearch from './VideoSearch.vue'
 
 export default {
   data () {
@@ -87,10 +131,18 @@ export default {
       directors: [],
       actors: [],
       target_review: null,
-      user_pk: ''
+      user_pk: '',
+      video: null,
     }
   },
-
+  computed: {
+    videoSrc: function () {
+      if (this.isSelected()) {
+        return "http://www.youtube.com/embed/" + this.video
+      }
+      return ''
+    },
+  },
   props: {
     movie: {
       type: Object,
@@ -101,9 +153,20 @@ export default {
       required: false,
     }
   },
-
+  components: {
+      VideoSearch,
+  },
   methods: {
+    isSelected: function () {
+      return Object.keys(this.video).length
+    },
+    goApp(video) {
+      this.video = null
+      this.video = video
+      console.log(this.video)
+    },
     closeDialog() {
+      this.video = null
       this.$emit('closeDialogEvent', true)
     },
 
